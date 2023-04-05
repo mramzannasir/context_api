a059de739b39c26bdfce66ef90fdea2da97cf245
Muhammad Ramzan N <mramzannasir555@gmail.com>
2023-04-05 11:12:48 +0500

    Done

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Layout = () => {
+	return (
+		<div className="w-full flex flex-col justify-center items-center">
+			<Navbar />
+		</div>
+	);
+};
+
+export default Layout;
